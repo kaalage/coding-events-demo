1fb5c9bcab15b5cacb2a87841a6463fdbdf30da1
Karina Lage <karinalage@karinas-air.lan>
2022-10-03 15:38:31 -0500

    Finished exercises Class 10 - Thymeleaf

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -17,11 +18,13 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
+        events.put("SLAM Underground", "The Saint Louis Art Museum collects, presents, interprets, and conserves works of art of the highest quality.");
+        events.put("One-Day Folk Fest", "Don’t miss this festival dedicated to folk music! Through live musical performances, activities for all ages.");
+        events.put("Zootoberfest", "Visit the Zoo for a free, family-friendly Oktoberfest!");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -33,8 +36,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
+    public String processCreateEventForm(@RequestParam String eventName, String eventDescription) {
+        events.put(eventName, eventDescription);
         return "redirect:";
     }
 
